e1119c635bdc65334c5489229f366d3f73f7ed5b
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-10 12:29:31 -0300

    Add files via upload

---
+++ phelp.js
@@ -0,0 +1,94 @@
+const phelp = (prefix) => {
+	return `
+║══════════════════════║
+                 🛠️ ⃟ ⃟ ⃟ ⃟ ⃟⛦𝑷𝒁𝑶⛥ ⃟ ⃟ ⃟ ⃟ ⃟🛠️                
+║══════════════════════║
+
+𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+
+                  ● ᏴϴͲ●ᎷᎬΝႮ● 
+
+𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+
+┌────────────❶────────────
+║
+║〘 INFORMAÇÕES 〙
+║
+╠  ● ᏴϴͲ●ᎷᎬΝႮ●     
+║    
+╠ *𝐃𝐎𝐍𝐎*  : ᴘᴀɪᴢᴀ̃ᴏ⁰²⁷
+║ https://Wa.me/+5527997179500
+║
+╠ *𝐃𝐎𝐍𝐎 * : ༒𝙵𝚞𝚌𝚔𝚃𝚁𝟹𝙼_៚༒
+║ https://Wa.me/+351926456359
+║
+╠ *𝐃𝐎𝐍𝐎*  : ༒ঔৣ͜͡𝐋𝐈𝐃𝐄𝐑 𝑴𝑨𝑹𝒀 𝑯𝑨𝑹𝑷𝑯𝒀𝑨༒⚡
+║ https://Wa.me/+552167166419 
+║
+║
+║
+║ *Tag* : 𝑷𝑪𝑾¹⁵⁷
+║ *Recado* : 𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+║
+║
+║
+╠══🤖〘 MENU PCW 〙🤖══║
+║
+║𝑷𝑪𝑾¹⁵⁷ *.pfigu* 
+║𝑷𝑪𝑾¹⁵⁷ *.ptoimg*
+║𝑷𝑪𝑾¹⁵⁷ *.txtfig*
+║𝑷𝑪𝑾¹⁵⁷ *.ping*
+║
+║
+║
+╠══🔥〘 GRUPO PCW〙🔥══║
+║
+║𝑷𝑪𝑾¹⁵⁷: *.banir*
+║𝑷𝑪𝑾¹⁵⁷: *.promover*
+║𝑷𝑪𝑾¹⁵⁷: *.rebaixar*
+║𝑷𝑪𝑾¹⁵⁷: *.admins*
+║𝑷𝑪𝑾¹⁵⁷: *.pcwall* [ para marcar todos ]
+║𝑷𝑪𝑾¹⁵⁷: *.pcwall2* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+║𝑷𝑪𝑾¹⁵⁷: *.pcwall3* [ para marcar todos ]
+║𝑷𝑪𝑾¹⁵⁷: *.bemvindo [1/0]*
+║𝑷𝑪𝑾¹⁵⁷: *.grupoinfo*
+║𝑷𝑪𝑾¹⁵⁷: *.bomdia*
+║𝑷𝑪𝑾¹⁵⁷: *.antilink*
+║
+║
+║
+╠══🤖〘 OUTROS 〙🤖══║
+║
+╠ 𝑷𝑪𝑾¹⁵⁷ Tag [para pegar a tag da 𝑷𝑪𝑾¹⁵⁷]
+╠𝑷𝑪𝑾¹⁵⁷ Recado [para pegar o recado da 𝑷𝑪𝑾¹⁵⁷]
+╠𝑷𝑪𝑾¹⁵⁷ Canal [para pegar o canal da 𝑷𝑪𝑾¹⁵⁷]
+║
+║
+║
+╠══🤖〘 PZO NO CONTROLE 〙🤖══║
+║
+║ 𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+║                 🛠️ ⃟ ⃟ ⃟ ⃟ ⃟⛦𝑷𝒁𝑶⛥ ⃟ ⃟ ⃟ ⃟ ⃟🛠️                   
+║ 𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+║
+║
+╠ Canal da PCW:
+║https://youtube.com/channel/UCR3if-pal1kZxnPp0hZMmWw
+║
+╠ Canal de Edit: 
+║https://youtube.com/channel/UCnzvrzBTDzzycCU0JxWah_g 
+║
+╠ Canal da MaryHarphya:
+║https://youtube.com/channel/UCeYswOOlot2mKghfcmj2VkQ 
+║
+╠ Canal do FuckTR3M:
+║https://youtube.com/channel/UCCXSfIsQVm1w1udGNW5hX5A
+║
+╠ Paizao Da Private:
+║https://youtube.com/channel/UCJHID_3QlOEVSrgW4F0s73Q 
+║
+╚═〘 PCW BOT 〙`
+}
+
+exports.phelp = phelp
+
