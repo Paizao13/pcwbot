37b61767c6eaa2ec39247c97a4c4caa47d3af538
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-09 23:59:06 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1582,63 +1582,6 @@ break
 					if (!isOwner) return reply('Você quem é o dono? ')
 					client.sendMessage(from, addfoto(prefix), text, { quoted: mek })
 					break
-					case 'base'
-					if (args.length < 1) return reply('𝐸 𝐴 𝑃𝐶𝑊 𝐾𝐴𝑅𝐴𝐿𝐻𝑂𝑂𝑂𝑂𝑂𝑂𝑂𝑂𝑂👺⚡
-
-𝑂 𝑇𝐸𝑅𝑅𝑂𝑅 𝐷𝑂𝑆 𝑅𝐴𝑀𝐷𝑂𝑁𝑆 𝐷𝐴 𝑉𝐼𝐷𝐴👺⚡
-
-𝐴 𝑂𝑅𝐺𝐴𝑁𝐼𝑍𝐴𝐶𝐴𝑂 𝑀𝐴𝐼𝑆 𝐵𝐴𝑅𝐵𝐴 𝐷𝐴 𝑊𝐸𝐵👺⚡
-
-𝑇𝑈𝐷𝑂 𝑄𝑈𝐸 𝑉𝑂𝐶𝐸 𝑃𝑅𝐸𝐶𝐼𝑆𝐴 𝐴𝑃𝑅𝐸𝑁𝐷𝐸𝑅 𝑆𝑂𝐵𝑅𝐸 𝐶𝑂𝑁𝐻𝐸𝐶𝐼𝑀𝐸𝑁𝑇𝑂𝑆  𝐸𝑀 𝑉𝐴𝑅𝐼𝐴𝑆 𝐴𝑅𝐸𝐴𝑆 𝑆𝑂 𝑁𝑂𝑆 𝐷𝐴 𝑃𝐶𝑊 𝐸𝑁𝑆𝐼𝑁𝐴𝑀𝑂𝑆
-
-𝐸 𝑉𝑂𝐶𝐸 𝑄𝑈𝐸 𝐸𝑆𝑇𝐴 𝐶𝐴𝑁𝑆𝐴𝐷𝑂 (𝐴) 𝐷𝐸𝑆𝑆𝐴 𝑉𝐼𝐷𝐴 𝑆𝐸𝑀 𝐹𝑈𝑇𝑈𝑅𝑂 𝐷𝐸 𝑇𝑅𝐴𝑉𝐴𝑅 𝐴𝑃𝐿𝐼𝐶𝐴𝑇𝐼𝑉𝑂 𝐷𝐸 𝑀𝐸𝑁𝑆𝐴𝐺𝐸𝑀 𝐾𝐾𝐾𝐾𝐾𝐾 𝑉𝐸𝑁𝐻𝐴 𝐹𝐴𝑍𝐸𝑅 𝑃𝐴𝑅𝑇𝐸 𝐷𝐴 𝑁𝑂𝑆𝑆𝐴 𝑂𝑅𝐺𝐴𝑁𝐼𝑍𝐴𝐶𝐴𝑂 𝐸 𝑆𝐸 𝑈𝑁𝐼𝑅 𝐴𝑂𝑆 𝑀𝐴𝐼𝑆 𝐵𝑅𝐴𝐵𝑂𝑆 𝐸 𝑅𝐸𝑆𝑃𝐸𝐼𝑇𝐴𝐷𝑂𝑆 𝐷𝐴 𝑊𝐸𝐵
-
-⏯️[𝑃𝐶𝑊 (𝑃𝑅𝐼𝑀𝐸𝐼𝑅𝑂 𝐶𝑂𝑀𝐴𝑁𝐷𝑂 𝐷𝐴 𝑊𝐸𝐵) 𝑆𝐸 𝐶𝐴𝐼𝑅 𝐴𝑄𝑈𝐼 𝐸 𝑉𝐸𝐿𝐴 𝐸 𝐶𝐴𝐼𝑋𝐴𝑂, 𝑃𝐸𝐼𝑇𝐴 𝑂𝑈 𝑅𝐸𝑆𝑃𝐸𝐼𝑇𝐴 𝑅𝐴𝑁𝐷𝑂𝑁𝑆 👺]
-
-APRENDAM UMA COISA....SE MEXER COM  A GENTE... CAIU  AQUI É VELA E CAIXÃO PARÇA, NÃO TEM CONVERSA
-
-PEITA OU RESPEITA KRLH
-
-LINK DO CANAL CRIA DA MARY HARPHYA PCW
-⏯️https://youtube.com/channel/UCeYswOOlot2mKghfcmj2VkQ
-
-LINK DO CANAL PRIMEIRO COMANDO DA WEB
-⏯️ https://youtu.be/IYGHwn7Ddq8
-
-LINK DA BASES GERAL 1
-⏯️
-https://chat.whatsapp.com/Ll3q0WAzEts3zbsaD4WABm
-
-BASE 2
-⏯️
-https://chat.whatsapp.com/JPDhGezacCM5dpZfTsGvuu
-
-LINK DA BASE 3
-⏯️
-https://chat.whatsapp.com/DTyhKIDg1i92baw41BXaB8
-
-LINK DA STUDIO PCW
-⏯️
-https://chat.whatsapp.com/HGF8YxtQHXo82LDfVDUAAO
-
-LINK DA BASE TERMUX 0.2
-⏯️
-https://chat.whatsapp.com/JkKQ1ESF6OFCQSyUTUGYlC
-
-LINK DA BASE ANTI BAN
-⏯️
-
-https://chat.whatsapp.com/HaamNJmS8scDnjWmRVDwRf
-
-INSTAGRAM OFICIAL DA PCW 💥
-https://instagram.com/primeiro_comando_da_web?igshid=anz90ebuicb3
-
-PCW DOMINA POHAAAAAA
-
-ATT:ꪶ͢ 𝐏𝐂𝐖͢ 𝐂𝐎𝐑𝐏𝐎𝐑𝐀𝐓𝐈𝐎𝐍͢ 𝐋𝐓𝐃𝐀
-
-ATT: 𝐃𝐈𝐑𝐄𝐓𝐎𝐑𝐈𝐀 𝐏𝐂𝐖 _𝐏𝐂𝐖¹⁵⁷')
-break
 			    case 'otagall3':
 					if (!isGroup) return reply(mess.only.group)
 					if (!isGroupAdmins) return reply(mess.only.admin)
