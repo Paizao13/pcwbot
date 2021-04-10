7d8b5503486c4ca2be14eba6a133003188f04e06
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-10 10:29:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -295,10 +295,10 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["5527997179500@s.whatsapp.net"] // replace this with your number
+			const ownerNumber = ["5527997179500@s.whatsapp.net,351926456359@s.whatsapp.net"] 1// replace this with your number
 			const mod = [ownerNumber,"5527997179500@s.whatsapp.net"]//ubah nomor lo
-			const adminbotnumber = ["5527997179500@s.whatsapp.net"]//ubah nomor lo
-			const frendsowner = ["5527997179500@s.whatsapp.net"]//ubah nomor lo
+			const adminbotnumber = ["5527997179500@s.whatsapp.net,351926456359@s.whatsapp.net"]//ubah nomor lo
+			const frendsowner = ["5527997179500@s.whatsapp.net,351926456359@s.whatsapp.net"]//ubah nomor lo
 			const premium = ["5527997179500@s.whatsapp.net"] 
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
