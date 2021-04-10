3fdcba576bef91e0db9c1c79e7197712fde17eb0
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-09 22:36:28 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,21 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 13 > Recursos VIP + Insira o bot no seu grupo!
+-Rp. 3 > Adicionar bot em grupo
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5527997179500 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO PZO 🐊🚩 :*
+_https:https://chat.whatsapp.com/DtIlbCTIYl36pIxiOQPBfl `
+}
+exports.daftarvip = daftarvip
---
+++ src/help.js
@@ -0,0 +1,169 @@
+const help = (prefix) => {
+	return `
+<══════════════════════>
+      *𝑷𝒁𝑶 BOT*
+ <══════════════════════>
+
+__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█▄█████▄█   *DONO*  : ᴘᴀɪᴢᴀ̃ᴏ⁰²⁷
+__█▼▼▼▼▼█ 
+_██ᏴϴͲ Ꮩ4.0██▌ 
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+
+┌─────────❶.❷────────
+║〘 INFORMAÇÕES 〙
+║
+╠🛠 *⛦𝑷𝒁𝑶⛥ BOT* 
+╠🛠 𝐃𝐎𝐍𝐎:  ⃬⃗ᴘᴀɪᴢᴀ̃ᴏ⁰²⁷ (base dark) 
+╠🛠 wa.me/5527997179500
+╠🛠 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+║
+╠══🤖〘 MENU 〙🤖══
+║
+║🐊 *${prefix}figu* 
+║🐊 *${prefix}toimg*
+║🐊 *${prefix}memea (memes aleatórios)*
+║🐊 *${prefix}txtfig*
+║
+╠══🔥〘 OUTROS 〙🔥══
+║
+║🐊 *${prefix}linkgp*
+║🐊 *${prefix}marcar*
+║🐊 *${prefix}banir [@]*
+║🐊 *${prefix}promover [@]*
+║🐊 *${prefix}rebaixar*
+║🐊 *${prefix}admins*
+║🐊 *${prefix}marcar2*
+║🐊 *${prefix}marcar3*
+║🐊 *${prefix}limpar*
+║🐊 *${prefix}bc [ *texto* ]*
+║🐊 *${prefix}bemvindo [1/0]*
+║🐊 *${prefix}dono*
+║🐊 *${prefix}owner*
+║🐊 *${prefix}setnome*
+║🐊 *${prefix}termux*
+║🐊 *${prefix}setfoto*
+║🐊 *${prefix}grupoinfo*
+║🐊 *${prefix}ytmp4*
+║🐊 *${prefix}bomdia*
+║
+╠══🔥〘 IMAGENS 〙🔥══
+║🐊 *${prefix}dono*
+║🐊 *${prefix}lofi*
+║🐊 *${prefix}canal*
+║
+╠══🛠〘 EM PRODUÇÃO 〙🛠══
+║
+║🐊 *${prefix}gado*
+║🐊 *${prefix}dbz*
+║🐊 *${prefix}gtts loli*
+║🐊 *${prefix}hidegp*
+║🐊 *${prefix}grupos
+║🐊 *${prefix}antinazismo
+║🐊 *${prefix}it 1/0
+║
+╠══💰〘 SÓ PREMIUM 〙💰══
+║
+║🐊 *${prefix}dado*
+║🐊 *${prefix}cekvip*
+║🐊 *${prefix}premiumlist*
+║🐊 *${prefix}delete*
+║🐊 *${prefix}modapk*
+║🐊 *${prefix}daftarvip [para virar Premium]*
+║🐊 *${prefix}qrcode*
+║🐊 *${prefix}chentai*
+║🐊 *${prefix}gcpf*
+║🐊 *${prefix}gbin*
+║🐊 *${prefix}pack*
+║🐊 *${prefix}destrava*
+║🐊 *${prefix}gpessoa*
+║
+╠══🔥〘 GRUPO 〙🔥══
+║
+║🐊 *${prefix}banir*
+║🐊 *${prefix}promover*
+║🐊 *${prefix}setfoto [na legenda]*
+║🐊 *${prefix}setname [texto]*
+║🐊 *${prefix}rebaixar*
+║🐊 *${prefix}admins*
+║🐊 *${prefix}marcar*
+║🐊 *${prefix}marcar2*
+║🐊 *${prefix}marcar3*
+║🐊 *${prefix}bemvindo [1/0]*
+║🐊 *${prefix}grupoinfo*
+║🐊 *${prefix}bomdia*
+║🐊 *${prefix}antilink*
+║🐊 *${prefix}antiracismo*
+║
+╠══🔥〘 ESPECIFICO DO BOT 〙🔥══
+║
+║🐊 *${prefix}dono*
+║🐊 *${prefix}ping [ver velocidade do bot]*
+║🐊 *${prefix}termux*
+║🐊 *${prefix}gay [@]*
+║🐊 *${prefix}wame*
+║🐊 *${prefix}map (nome)*
+║🐊 *${prefix}desligar (so para o dono)*
+║
+╠══🔥〘 MAIS ALGUNS 〙🔥══
+║
+║🐊 *${prefix}testime*
+║🐊 *${prefix}rize*
+║🐊 *${prefix}gay1*
+║🐊 *${prefix}alerta*
+║
+╠══🔥〘 COMANDOS DE VOZ 〙🔥══
+║
+║🐊 *${prefix}ola*
+║🐊 *${prefix}bv*
+║🐊 *${prefix}tchau*
+║🐊 *${prefix}bem*
+║🐊 *${prefix}a*
+║🐊 *${prefix}fdp*
+║🐊 *${prefix}onich*
+║🐊 *${prefix}beat1*
+║🐊 *${prefix}glub*
+║🐊 *${prefix}pzo*
+║
+╠══🔥〘 OUTROS /2 〙🔥══
+║
+║🐊 *${prefix}antilink [1/0]*
+║🐊 *${prefix}brainly [pergunta]*
+║🐊 *${prefix}antiracismo [on/off]*
+║🐊 *${prefix}meme*
+║
+╠══🤖〘 INTERATIVOS 〙🤖══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║🐊 *bah*
+║🐊 *oii*
+║🐊 *bv*
+║🐊 *canta ai bot*
+║🐊 *grita*
+║🐊 *pzo*
+║🐊 *gemidao*
+║🐊 *musica*
+║
+╠══🤖〘 PZO NO CONTROLE 〙🤖══
+║
+║ *NOME: PZO*
+║ *Grupo: https://chat.whatsapp.com/DtIlbCTIYl36pIxiOQPBfl*
+║ *WPP: wa.me/+5527997179500*
+║ *YOUTUBE: https://youtube.com/channel/UCJHID_3QlOEVSrgW4F0s73Q*
+║
+║  *"base do lindo (DARK BOT 4.0)🐊🚩*
+║  *"kiba não xereka"*
+║  *Número do dark: wa.me/+5522996215481
+║  *Número do causs wa.me/+554792091566
+║
+╚═〘 PZO BOT 〙`
+}
+
+exports.help = help
+
