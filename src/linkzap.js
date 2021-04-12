411027a46e29196db7b57a3f27addaf521b9e5fe
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-12 03:26:05 -0300

    Add files via upload

---
+++ src/linkzap.js
@@ -0,0 +1,16 @@
+const linkzap = (prefix) => {
+	return `
+🚀salva zap🚀
+
+😎Me chamo ${pushname} 
+
+🌹client.updatePresence(from, Presence.composing) 
+                  options = {🌹
+🌞　　　　🛸　　　 　🌎　°　　🌓　•　　.°•　　　🚀 ✯✯　　
+　　　★　*　　　　　°　　　　🛰　°·　　
+.　　　•　°★　•
+▁▂▃▄▅▆▇▇▆▅▄▃🌲Quiser Trocar divulgação vai no meu pv
+*https://api.whatsapp.com/send?phone=${sender.split("@")[0]}*`
+}
+
+exports.linkzap = linkzap
