e5246f335667345a70502b4c2e5c70b2c93133e0
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-12 01:15:33 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,94 @@
+const help = (prefix) => {
+	return `
+⛥ ═════════════════════ ⛥
+                    *𝑷𝑪𝑾¹⁵⁷ - BOT*
+⛥ ═════════════════════ ⛥ 
+     
+⠀⠀⠀⠀⠀.      ⣷　　　　　　
+ ⢄⡀⠀⠀⠀⠀⠀⣿⣿⡄⠀⠀⠀⠀⠀⣠⠂ ⠀
+   ⢻⣶⣄⠀⠀⢀⣿⣿⡇⠀⠀⣠⣴⣾⠃ ⠀⠀
+     ⢻⣿⣷⡄⢸⣿⣿⡇⢀⣾⣿⣿⠏ ⠀    
+  ⠀⠀⠙⢿⣿⡄⢿⡿⣰⣿⣿⠟⠃          
+    ⠐⠶⣶⣶⣦⣍⠛⠜⠳⢛⣫⣵⣶⣾⠿⠋⠉ ⠀⠀
+        ⠉⣩⡽⠗⢡⠙⢯⣭⣉⠉
+
+*DONO: ᴘᴀɪᴢᴀ̃ᴏ⁰²⁷*
+*BOT: 𝑷𝑪𝑾¹⁵⁷ V2*
+┌────────❷─────────
+║      〘 *INFORMAÇÕES* 〙
+╠🛠 *⛦𝑷𝑪𝑾¹⁵⁷⛥ BOT* 
+╠🛠 𝐃𝐎𝐍𝐎:  ⃬⃗ᴘᴀɪᴢᴀ̃ᴏ⁰²⁷ 
+╠🛠 wa.me/5527997179500
+╠🛠 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠═══⚔️ 〘 MENU 〙⚔️════
+╠➥ *${prefix}tag*
+╠➥ *${prefix}recado*
+║
+╠➥ *${prefix}pfig*
+╠➥ *${prefix}ptoimg*
+╠➥ *${prefix}txtfig*
+║
+╠═══⚔️〘 GRUPO 〙⚔️════
+╠➥ *${prefix}antilink [somente adm]
+╠➥ *${prefix}antiracismo [somente adm]*
+╠➥ *${prefix}admins* 
+╠➥ *${prefix}banir [@]* 
+╠➥ *${prefix}bemvindo [1/0]* 
+╠➥ *${prefix}linkgp [somente adm]* 
+╠➥ *${prefix}pcwall* 
+╠➥ *${prefix}pcwall2*
+╠➥ *${prefix}pcwall3*
+╠➥ *${prefix}promover [@]*
+╠➥ *${prefix}rebaixar [@]*
+╠➥ *${prefix}setnome [somente adm]*
+╠➥ *${prefix}setfoto [na legenda] - [somente adm]*
+║
+╠═══⚔️〘 MÚSICA 〙⚔️════
+╠➥ *${prefix}play*
+║
+╠═══⚔️〘 OUTROS 〙⚔️════
+╠➥ *${prefix}bot*
+╠➥ *${prefix}canal*
+╠➥ *${prefix}lofi*
+╠➥ *${prefix}termux*
+╠➥ *${prefix}wa.me*
+║
+╠═══⚔️〘 BOT INFOS〙⚔️════
+╠➥ *${prefix}ping [ver velocidade do bot]*
+╠➥ *${prefix}dono*
+║
+╠═══⚔️〘 INTERATIVOS 〙⚔️════
+╠➥ *bot lindo*
+╠➥ *eae bot*
+╠➥ *fdp*
+╠➥ *obgd bot*
+╠➥ *vtmnc*
+║
+╠═══ ⚔️ 〘 PZO NO CONTROLE 〙⚔️════
+║
+║      𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+║                     🛠️ ⃟ ⃟ ⃟ ⃟ ⃟⛦𝑷𝑪𝑾¹⁵⁷⛥ ⃟ ⃟ ⃟ ⃟ ⃟🛠️                   
+║      𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+║
+║
+╠➥ Canal da PCW:
+║https://youtube.com/channel/UCR3if-pal1kZxnPp0hZMmWw
+║
+╠➥ Canal de Edit: 
+║https://youtube.com/channel/UCnzvrzBTDzzycCU0JxWah_g 
+║
+╠➥ Canal da MaryHarphya:
+║https://youtube.com/channel/UCeYswOOlot2mKghfcmj2VkQ 
+║
+╠➥ Canal do FuckTR3M:
+║https://youtube.com/channel/UCCXSfIsQVm1w1udGNW5hX5A
+║
+╠➥ Paizao Da Private:
+║https://youtube.com/channel/UCJHID_3QlOEVSrgW4F0s73Q 
+║
+╚═〘 PCW BOT 〙`
+}
+
+exports.help = help
+
