be2dfd3db97eb33910307a06a1fdeee2ccb739b6
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-20 14:20:46 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,104 @@
+const help = (prefix) => {
+	return `
+⛥ ═════════════════════ ⛥
+                    *𝑷𝑪𝑾¹⁵⁷ - BOT*
+⛥ ═════════════════════ ⛥ 
+     
+. █▀▀█ █▀▀█ █░░▒█
+. █▄▄█ █░░░ █▒█▒█
+. █░░░ █▄▄█ █▄▀▄█
+
+*DONO: ᴘᴀɪᴢᴀ̃ᴏ⁰²⁷*
+*BOT: 𝑷𝑪𝑾¹⁵⁷ V3*
+┌────────❸─────────
+║      〘 *INFORMAÇÕES* 〙
+╠🛠 *⛦𝑷𝑪𝑾¹⁵⁷⛥ BOT* 
+╠🛠 𝐃𝐎𝐍𝐎:  ⃬⃗ᴘᴀɪᴢᴀ̃ᴏ⁰²⁷ 
+╠🛠 wa.me/5527997179500
+╠🛠 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠═══⚔️ 〘 Tag/Recado 〙⚔️════
+╠➥ *tag*
+╠➥ *recado*
+║
+╠═══⚔️ 〘 MENU 〙⚔️════
+║
+╠➥ *${prefix}sticker*
+╠➥ *${prefix}converter* ( converter fig em imagem )
+╠➥ *${prefix}txtfig*
+║
+╠═══⚔️〘 GRUPO 〙⚔️════
+╠➥ *${prefix}antlink [1/0] [somente adm]*
+╠➥ *${prefix}antracismo [on/off] [somente adm]*
+╠➥ *${prefix}admins* 
+╠➥ *${prefix}base [somente adm]*
+╠➥ *${prefix}banir [@] [somente adm]*
+╠➥ *${prefix}bemvindo [1/0] [somente adm]*
+╠➥ *${prefix}linkgp [somente adm]* 
+╠➥ *${prefix}pcwall [somente adm]*
+╠➥ *${prefix}pcwall2 [somente adm]*
+╠➥ *${prefix}pcwall3 [somente adm]*
+╠➥ *${prefix}pcwall4 [somente adm]*
+╠➥ *${prefix}promover [@] [somente adm]*
+╠➥ *${prefix}rebaixar [@] [somente adm]*
+╠➥ *${prefix}setnome [somente adm]*
+╠➥ *${prefix}setfoto [na legenda] - [somente adm]*
+║
+╠═══⚔️〘 MÚSICA 〙⚔️════
+╠➥ *${prefix}play*
+║
+╠═══⚔️〘 OUTROS 〙⚔️════
+╠➥ *${prefix}bot*
+╠➥ *${prefix}canal*
+╠➥ *${prefix}lofi*
+╠➥ *${prefix}pcwinsta*
+╠➥ *${prefix}termux*
+╠➥ *${prefix}wa.me*
+║
+╠═══⚔️〘 BOT INFOS〙⚔️════
+╠➥ *${prefix}ping [ver velocidade do bot]*
+╠➥ *${prefix}dono*
+║
+╠═══⚔️〘 INTERATIVOS 〙⚔️════
+╠➥ *bot*
+╠➥ *canal*
+╠➥ *eae*
+╠➥ *fdp*
+╠➥ *kdobot*
+╠➥ *obgd*
+╠➥ *pcw*
+╠➥ *vtmnc*
+╠➥ *vsfd*
+║
+╠═══⚔️ 〘 DONO 〙⚔️════
+╠➥ *${prefix}ptm* (tm de avisa)
+╠➥ *${prefix}pt2* (tm de divulgação)
+╠➥ *${prefix}pt3* (tm admin)
+║
+╠═══ ⚔️ 〘 PZO NO CONTROLE 〙⚔️════
+║
+║      𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+║                     🛠️ ⃟ ⃟ ⃟ ⃟ ⃟⛦𝑷𝑪𝑾¹⁵⁷⛥ ⃟ ⃟ ⃟ ⃟ ⃟🛠️                   
+║      𓇽͢ 𝑷𝑹𝑰𝑴𝑬𝑰𝑹𝑶͢ 𝑪𝑶𝑴𝑨𝑵𝑫𝑶͢ 𝑫𝑨͢ 𝑾𝑬𝑩͢ ¹⁵⁷
+║
+║
+╠➥ Canal da PCW:
+║https://youtube.com/channel/UCR3if-pal1kZxnPp0hZMmWw
+║
+╠➥ Canal de Edit: 
+║https://youtube.com/channel/UCnzvrzBTDzzycCU0JxWah_g 
+║
+╠➥ Canal da MaryHarphya:
+║https://youtube.com/channel/UCeYswOOlot2mKghfcmj2VkQ 
+║
+╠➥ Canal do FuckTR3M:
+║https://youtube.com/channel/UCCXSfIsQVm1w1udGNW5hX5A
+║
+╠➥ Paizao Da Private:
+║https://youtube.com/channel/UCJHID_3QlOEVSrgW4F0s73Q 
+║
+╚═〘 PCW BOT 〙`
+}
+
+exports.help = help
+
