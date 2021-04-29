6aba75e63e862b7cf950d0f8371bdfe18fbc93ce
Paizao13 <82244799+Paizao13@users.noreply.github.com>
2021-04-29 17:18:45 -0300

    Add files via upload

--- src/base.js
+++ src/base.js
@@ -6,20 +6,23 @@ const base = (prefix) => {
 
 ༒♞𝐁𝐀𝐒𝐄  𝐖𝐎𝐌𝐀𝐍𝐒  𝐏𝐂𝐖¹⁵⁷ 
 
-ꪶ𝓽𝐁∆𝐒𝐄 𝐆𝐄𝐑∆𝐋 𝐏𝐂𝐖⓿❷https://chat.whatsapp.com/LotwKjnIjdK9JAccActjOb
+ꪶ𝓽𝐁∆𝐒𝐄 𝐆𝐄𝐑∆𝐋 𝐏𝐂𝐖⓿❶ ➔ https://chat.whatsapp.com/Ll3q0WAzEts3zbsaD4WABm
 
+ꪶ𝓽𝐁∆𝐒𝐄 𝐆𝐄𝐑∆𝐋 𝐏𝐂𝐖⓿❷ ➔ https://chat.whatsapp.com/LotwKjnIjdK9JAccActjOb
 
-ꪶ𝓽𝐁∆𝐒𝐄 𝐆𝐄𝐑∆𝐋 𝐏𝐂𝐖⓿❸https://chat.whatsapp.com/CQwo5BmpY8T6zFbhPVF3UN
+ꪶ𝓽𝐁∆𝐒𝐄 𝐆𝐄𝐑∆𝐋 𝐏𝐂𝐖⓿❸ ➔ https://chat.whatsapp.com/CQwo5BmpY8T6zFbhPVF3UN
 
-𝑩𝑨𝑺𝑬 𝑨𝑵𝑻𝑰 𝑩𝑨𝑵_𝐏𝐂𝐖¹⁵⁷https://chat.whatsapp.com/HaamNJmS8scDnjWmRVDwRf
+ꪶ𝓽𝐁∆𝐒𝐄 𝐆𝐄𝐑∆𝐋 𝐏𝐂𝐖⓿❹ ➔ https://chat.whatsapp.com/Hqd7XnjGT3mBkeBSmNOnwF {NOVA BASE}
 
-𓇽͢ 𝐒𝐓𝐔𝐃𝐈𝐎𝐒 𝐏𝐂𝐖¹⁵⁷https://chat.whatsapp.com/HGF8YxtQHXo82LDfVDUAAO
+𝑩𝑨𝑺𝑬 𝑨𝑵𝑻𝑰 𝑩𝑨𝑵_𝐏𝐂𝐖¹⁵⁷ ➔ https://chat.whatsapp.com/HaamNJmS8scDnjWmRVDwRf
 
-𓇽͢ .𝐒𝐓𝐔𝐃𝐈𝐎𝐒 𝑷𝑪𝑾⓿➋https://chat.whatsapp.com/IvsOAjZohBJDkeZMpDQkK1
+𓇽͢ 𝐒𝐓𝐔𝐃𝐈𝐎𝐒 𝐏𝐂𝐖¹⁵⁷ ➔ https://chat.whatsapp.com/HGF8YxtQHXo82LDfVDUAAO
 
-ꪶ𝓽𝐁∆𝐒𝐄 𝑻𝑬𝑹𝑴𝑼𝑿 ≥https://chat.whatsapp.com/CnM0ZcHwSfD7pkwISW93B0
+𓇽͢ .𝐒𝐓𝐔𝐃𝐈𝐎𝐒 𝑷𝑪𝑾⓿➋ ➔ https://chat.whatsapp.com/IvsOAjZohBJDkeZMpDQkK1
 
-ꪶ𝓽𝐁∆𝐒𝐄 𝑻𝑬𝑹𝑴𝑼𝑿 ≥⓿❷https://chat.whatsapp.com/JkKQ1ESF6OFCQSyUTUGYlC
+ꪶ𝓽𝐁∆𝐒𝐄 𝑻𝑬𝑹𝑴𝑼𝑿 ≥ ➔ https://chat.whatsapp.com/CnM0ZcHwSfD7pkwISW93B0
+
+ꪶ𝓽𝐁∆𝐒𝐄 𝑻𝑬𝑹𝑴𝑼𝑿 ≥⓿❷ ➔ https://chat.whatsapp.com/JkKQ1ESF6OFCQSyUTUGYlC
 
 
 ➡️INSTAGRAM OFICIAL :
@@ -28,8 +31,7 @@ https://instagram.com/primeiro_comando_da_web?igshid=anz90ebuicb3
 ➡️CANAL PCW:
 https://youtube.com/channel/UCR3if-pal1kZxnPp0hZMmWw
 
-➡️CANAL MARY KHARPHIA
-https://youtube.com/channel/UCeYswOOlot2mKghfcmj2VkQ`
+➡️CANAL MARY KHARPHIA: https://youtube.com/channel/UCeYswOOlot2mKghfcmj2VkQ`
 }
 
 exports.base = base
